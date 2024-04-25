c2e38d2c55288281cae4c3701a00ad7c960c577b
MOHIT SAINI <84871607+memohit2@users.noreply.github.com>
2024-04-26 01:09:35 +0530

    lwcFileUpload.js:

--- LWC_File_Upload_Component/File_upload.js
+++ LWC_File_Upload_Component/File_upload.js
@@ -1,1 +1,22 @@
-  absdbaja
+import {LightningElement, api } from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+export default class LwcFileUpload extends LightningElement {
+  
+@api
+  myRecordId;
+get acceptedFileItem() {
+     return['.pdf', '.png', '.pdf'];
+   }
+ 
+uploadFiledAction(event) {
+//get the list of Uploaded files
+    const uploadFiles = event.detail.files;
+// alert("No.of Files Uploaded:")+ uploadedFiles.length);
+const toastEvent = new showToastEvent({
+      title:'File Uploaded Successfully!',
+      message:'No. of Files Uploaded '+ uploadedFiles.length,
+      variant:'Success',
+})
+    this.dispatchEvent(toastEvent);
+  }
+}
